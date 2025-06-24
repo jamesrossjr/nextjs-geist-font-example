aa2f61de20878958f5d3537fb3dc8c1025b72a14
jamesrossjr <58033936+jamesrossjr@users.noreply.github.com>
2025-06-24 16:30:30 -0500

    Add .next/static/chunks/src_app_dashboard_page_tsx_8d6d5d58._.js 1750800629290

---
+++ .next/static/chunks/src_app_dashboard_page_tsx_8d6d5d58._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_27dc21f9._.js",
+  "static/chunks/src_b3d8055f._.js"
+],
+    source: "dynamic"
+});
